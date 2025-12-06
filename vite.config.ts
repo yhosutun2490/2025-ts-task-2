ec02cf89fe15ddff4d334078098db4fd61272193
yhosutun2491 <yhosutun2491@gmail.com>
2025-12-06 13:53:14 +0800

    chore: 安裝gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { fileURLToPath, resolve, URL } from 'node:url'
-
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
