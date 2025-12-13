7348c9da340d9c9bb26ea8488a8ca10114af22d7
yhosutun2491 <yhosutun2491@gmail.com>
2025-12-13 15:41:31 +0800

    feat:　增加購物車型別

---
+++ src/types/cart.ts
@@ -0,0 +1,50 @@
+import { type ProductItem } from '@/types/product'
+import { type CouponData } from '@/types/coupon'
+
+
+export type GetCartResponse = {
+  success: boolean
+  data: {
+    carts: {
+      coupon: CouponData | null
+      final_total: number
+      id: string
+      product: ProductItem
+      product_id: string
+      qty: number
+      total: number
+    }[]
+    total: number
+    final_total: number
+  }
+  messages: unknown[]
+}
+
+export type AddCartItemResponse = {
+  success: boolean
+  message: string
+  data: {
+    product_id: string
+    qty: number
+    id: string
+    total: number
+    final_total: number
+    product: ProductItem
+  }
+}
+
+export type UpdateCartItemResponse = {
+  success: boolean
+  message: string
+  data: {
+    product_id: string
+    qty: number
+  }
+}
+
+export type DeleteCartItemResponse = {
+  success: boolean
+  message: string
+}
+
+
